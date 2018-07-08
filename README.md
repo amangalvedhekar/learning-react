251d499ca3bc2111279afec8dc0c2717935c2588
Anirudh <amangalvedhekar@icloud.com>
2018-07-07 20:31:27 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
+# Learning React:
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+### Component
+
+* Component Types:
+    * Component Using ` class `
+    * Component Using ` function `
+* Lifecycle of component:
+    * Mounting
+    * Updating
+    * Unmounting
+
+* Testing:
+
+
+
+### Props
+
+### State
--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="jumbotron">
+          <p>
+              Author quiz
+          </p>
+        </div>
       </div>
     );
   }
